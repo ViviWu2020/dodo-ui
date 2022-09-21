fca3aad2eb8314bce60b238a28e5c0307e7f071d
Vivi <jiahui1001@yeah.net>
2022-09-21 20:44:36 +0800

    改造 ButtonDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,18 +7,15 @@
 </template>
 
 <script>
-import { ref } from "vue";
-
-import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
 import Demo from "./Demo.vue";
 
 export default {
-  components: { Button, Demo },
+  components: { Demo },
   setup() {
-    const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo, Prism };
+
+    return { Switch1Demo, Switch2Demo, Prism };
   },
 };
 </script>
